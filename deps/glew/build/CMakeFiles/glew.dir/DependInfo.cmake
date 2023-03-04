5946bac460d9b0ccf44d6992c47f01731d471eb8
Eddie Breeg <eddiebreeg0@protonmail.com>
2023-03-04 16:46:30 -0500

    added glew build dir

---
+++ deps/glew/build/CMakeFiles/glew.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/eddie/Downloads/glew-2.2.0/src/glew.c" "CMakeFiles/glew.dir/src/glew.c.o" "gcc" "CMakeFiles/glew.dir/src/glew.c.o.d"
+  "/home/eddie/Downloads/glew-2.2.0/src/glewinfo.c" "CMakeFiles/glew.dir/src/glewinfo.c.o" "gcc" "CMakeFiles/glew.dir/src/glewinfo.c.o.d"
+  "/home/eddie/Downloads/glew-2.2.0/src/visualinfo.c" "CMakeFiles/glew.dir/src/visualinfo.c.o" "gcc" "CMakeFiles/glew.dir/src/visualinfo.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
